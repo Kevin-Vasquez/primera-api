2269508f0d75cc22b1aaa7e655b6879cc6c5a3b6
Kevin-Vasquez <kevin1995vasquezz@gmail.com>
2024-04-30 21:01:35 -0600

    modificaiones en controllers

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,10 @@ class Product extends Model
     //aca definimos los campo de la migration que seran rellenables
     protected $fillable = ['name', 'description', 'price', 'stock', 'image', 'id_categoria', 'id_marca'];
 
+
+    //aca agregamos esta funcion para ver el nombre de la categoria
+    public function categoria(){
+        return $this->hasOne(Categoria::class, 'id', 'id_categoria');
+    }
+
 }
